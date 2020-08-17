342b0ddc1b0223b35716acfe179e6aa1cb334fa6
Jeremy Ong <jeremycong@gmail.com>
2020-08-16 22:42:55 -0600

    Move assets to the root

---
+++ precache-manifest.6136a241f3ccc46490febd9c61e6b74a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55742a2298613433cbe81a04d6c31a32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4211143d8c4e2ff0fb1",
+    "url": "/static/css/main.86f8143a.chunk.css"
+  },
+  {
+    "revision": "fe5437628fb250de4cab",
+    "url": "/static/js/2.fd1a1486.chunk.js"
+  },
+  {
+    "revision": "22480a5ae18e3ad56d4d27d8d52e2a45",
+    "url": "/static/js/2.fd1a1486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4211143d8c4e2ff0fb1",
+    "url": "/static/js/main.2069de0a.chunk.js"
+  },
+  {
+    "revision": "26dd8d81d0153a4332e5",
+    "url": "/static/js/runtime-main.e05c5361.js"
+  },
+  {
+    "revision": "059fe3bfec635ff15f251e5f407ffa51",
+    "url": "/static/media/github-actions.059fe3bf.jpg"
+  }
+]);
