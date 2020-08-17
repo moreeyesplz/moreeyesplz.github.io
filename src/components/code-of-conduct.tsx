531d514f96b416e066e0b229f9b6c1e54747cdab
Jeremy Ong <jeremycong@gmail.com>
2020-08-17 01:17:24 -0600

    Update code of conduct

--- src/components/code-of-conduct.tsx
+++ src/components/code-of-conduct.tsx
@@ -8,7 +8,7 @@ const useStyles = makeStyles((theme) => ({
     },
     "button-container": {
         display: "flex",
-        alignItems: 'flex-end', 
+        alignItems: 'flex-end',
         justifyContent: "center",
         padding: theme.spacing("16px", 0, "16px", 0),
     },
@@ -26,31 +26,52 @@ const useStyles = makeStyles((theme) => ({
 
 export default function UserStats() {
     const classes = useStyles();
-    
-    return(
+
+    return (
         <Paper elevation={0} className={classes.container}>
             <Typography variant="h5">✨ Code of Conduct</Typography>
             <div className={classes.divider}></div>
-            <Typography>MEEP! Welcome to more eyes, plz!</Typography> 
-            <br/>
-            <Typography>Lorizzle crackalackin shiznit sit amizzle, doggy adipiscing elit. Nullizzle fo shizzle velizzle, uhuh ... yih! go to hizzle, bow wow wow things, gravida for sure, crunk. Brizzle shiznit tortizzle. Fo shizzle my nizzle erizzle. Pizzle izzle break yo neck, yall hizzle fizzle tempus tempor.</Typography>
-            <br/>
-            <Typography>Mauris pellentesque boom shackalack turpizzle:</Typography>
-            <List>  
+            <Typography variant="h6">Offering a set of eyes?</Typography>
+            <br />
+            <List>
                 <ListItem disableGutters>
-                    1. Shizzlin dizzle izzle yo mamma.
+                    1. Offer feedback and advice graciously
                 </ListItem>
                 <ListItem disableGutters>
-                    2. Pellentesque pot rhoncizzle i saw beyonces tizzles and my pizzle went crizzle.
+                    2. Give justifications for why something should be done a particular way
                 </ListItem>
                 <ListItem disableGutters>
-                    3. In hac habitasse platea dictumst.
+                    3. Exhibit patience and remember that asking for help is in and of itself an act of courage
                 </ListItem>
             </List>
-            <Typography>Izzle dapibizzle. Fo shizzle tellizzle urna, pretizzle eu, gangster break yo neck, yall, eleifend mammasay mammasa mamma oo sa, nunc. Sheezy suscipizzle. Crackalackin semper crazy sizzle purus. Shut the shizzle up interdum tellus. Ut gizzle adipiscing lorem. Fo shizzle mah nizzle fo rizzle, mah home g-dizzle non est. Sheezy sapizzle fizzle, ultricizzle fo shizzle mah nizzle fo rizzle, mah home g-dizzle, own yo' gangster, fermentizzle quizzle, . Funky fresh nizzle boofron. </Typography>
-            <div className={classes["button-container"]}>
-                <Button className={classes.button} variant="outlined" color="primary" fullWidth disableElevation>Read More</Button>
-            </div>
+            <br />
+            <Typography variant="h6">Requesting another set of eyes?</Typography>
+            <br />
+            <List>
+                <ListItem disableGutters>
+                    1. Respect the time of people offering feedback
+                </ListItem>
+                <ListItem disableGutters>
+                    2. Avoid getting defensive
+                </ListItem>
+                <ListItem disableGutters>
+                    3. Understand that those giving you feedback are not perfect
+                </ListItem>
+            </List>
+            <br />
+            <Typography variant="h6">General Commit Thread Rules</Typography>
+            <br />
+            <List>
+                <ListItem disableGutters>
+                    1. Refrain from posting any content that is NSFW, racist, profane, or discriminatory toward any people group
+                </ListItem>
+                <ListItem disableGutters>
+                    2. Avoid posting any personal or identifying information of yourself or others
+                </ListItem>
+                <ListItem disableGutters>
+                    3. Do not post any recruitment messages, spam, or ads
+                </ListItem>
+            </List>
         </Paper>
     )
 }
