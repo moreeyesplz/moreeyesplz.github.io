f413a88d37a4990b66dbdfe4342337cc94fdc56a
Jeremy Ong <jeremycong@gmail.com>
2020-08-16 22:41:16 -0600

    Root commit

---
+++ build/precache-manifest.af0ccb11c5b6af6fb08fe2804e7060d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7f445d3500253472053749bdcd4692",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc251d857c740d2e503b",
+    "url": "/static/css/main.86f8143a.chunk.css"
+  },
+  {
+    "revision": "c661746ecc19fbf85c18",
+    "url": "/static/js/2.60e80cec.chunk.js"
+  },
+  {
+    "revision": "22480a5ae18e3ad56d4d27d8d52e2a45",
+    "url": "/static/js/2.60e80cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc251d857c740d2e503b",
+    "url": "/static/js/main.c1bf1430.chunk.js"
+  },
+  {
+    "revision": "26dd8d81d0153a4332e5",
+    "url": "/static/js/runtime-main.e05c5361.js"
+  }
+]);
